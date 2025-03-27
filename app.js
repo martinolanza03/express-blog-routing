25313fafe5b68afec90c249c36c152c38a212afe
martinolanza03 <lanzamartino3@gmail.com>
2025-03-27 15:42:13 +0100

    feat: change name folder, add get Read in the posts.js, exports router in app.js and create server

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const postsRouter = require('./routers/posts');
+
+//Static asset
+app.use(express.static('public'));
+
+//Call routes in posts.js
+app.use('/posts', postsRouter);
+
+//Server port
+app.listen(port, () => {
+    console.log('Server avviato');
+});
